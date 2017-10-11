a7effe0b0678407f6a4485cc89d93b8ad19761ea
saveryanov <railtrailtime@gmail.com>
2017-10-11 22:46:12 +0300

    binary search update
    Доработан алгоритм преобразования с бинарным поиском, обновлены тесты и index.js

--- index.js
+++ index.js
@@ -6,17 +6,35 @@
 
 var n2s = require('numbers-to-string'); // подгружаем модуль n2s
 
-var numbers = [1,2,3,7,8,9,15,17,19,20,21];	// массив чисел для преобразования
+var numbers = [1, 2, 3, 5, 6, 10, 20, 21, 22, 23, 1000];	// массив чисел для преобразования
+
+// Простое решение:
 
 // асинхронный запуск
 // функция, которая будет запущена после асинхронного выполнения
-//var callback = function(str) {
-//	console.log('Async.');
-//	console.log(str);
-//}
-//n2s.n2s(numbers, 0, callback);
+var callback_simple = function(str) {
+	console.log('Async.');
+	console.log(str);
+}
+n2s.n2s(numbers, callback_simple);
 
 // синхронный запуск
-var str = n2s.n2s(numbers, 1);				
+var str = n2s.n2s(numbers);				
 console.log('Sync.');
+console.log(str);
+
+
+// Решение с бинарным поиском
+
+// асинхронный запуск
+// функция, которая будет запущена после асинхронного выполнения
+var callback_binary = function(str) {
+	console.log('Async. Binary search.');
+	console.log(str);
+}
+n2s.n2s_bin(numbers, callback_binary);
+
+// синхронный запуск
+var str = n2s.n2s_bin(numbers);				
+console.log('Sync. Binary search.');
 console.log(str);
